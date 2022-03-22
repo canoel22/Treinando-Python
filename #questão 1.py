f71c37a013f781dd3a1397ab0a9c698ff25bd769
Carine Noel <86012254+canoel22@users.noreply.github.com>
2022-03-22 17:19:58 -0300

    Add files via upload

---
+++ #questão 1.py
@@ -0,0 +1,9 @@
+ #questão 1
+segundos= int(input("Quantos segundos?"))
+
+horas = segundos // 3600
+segundos_resto = segundos % 3600
+minutos = segundos_resto // 60
+segundos_resto = segundos_resto % 60
+
+print (horas, " horas, ", minutos, "minutos e ", segundos_resto, " segundos ")
